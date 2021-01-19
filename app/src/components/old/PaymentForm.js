2edb4f2b0c1c88ef5e55cfdc66918647dfa074da
D.Ogunsakin <ogunsakin191@gmail.com>
2021-01-19 02:58:57 +0100

    Format the components

--- app/src/components/old/PaymentForm.js
+++ app/src/components/old/PaymentForm.js
@@ -8,37 +8,37 @@ const { Checkbox, Input } = Form;
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 
 export default function PaymentForm() {
-    return (
-        <React.Fragment>
-            <Typography variant="h6" gutterBottom>
-                Payment method
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Payment method
             </Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12} md={6}>
-                    <Input required id="cardName" label="Name on card" fullWidth />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Input required id="cardNumber" label="Card number" fullWidth />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Input required id="expDate" label="Expiry date" fullWidth />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Input
-                        required
-                        id="cvv"
-                        label="CVV"
-                        helperText="Last three digits on signature strip"
-                        fullWidth
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <FormControlLabel
-                        control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-                        label="Remember credit card details for next time"
-                    />
-                </Grid>
-            </Grid>
-        </React.Fragment>
-    );
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Input required id="cardName" label="Name on card" fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Input required id="cardNumber" label="Card number" fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Input required id="expDate" label="Expiry date" fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Input
+            required
+            id="cvv"
+            label="CVV"
+            helperText="Last three digits on signature strip"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
+            label="Remember credit card details for next time"
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
