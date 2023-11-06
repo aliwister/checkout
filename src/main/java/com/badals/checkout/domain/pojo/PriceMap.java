8090a3b9f902a0b190ebcb9a4466f2d632fba149
Ebi <tmpmailebi@gmail.com>
2023-11-06 23:40:21 +0330

    added priceMap

---
+++ src/main/java/com/badals/checkout/domain/pojo/PriceMap.java
@@ -0,0 +1,29 @@
+package com.badals.checkout.domain.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class PriceMap implements Serializable {
+    private Map<String, String> prices = new HashMap<>();
+    private String base;
+
+    public String getPriceForCurrency(String code) {
+        return prices.get(code);
+    }
+
+    public void push(String currency, BigDecimal amount) {
+        prices.put(currency, amount.toPlainString());
+    }
+
+    public PriceMap(String base) {
+        this.base = base;
+    }
+
+    public PriceMap() {
+    }
+}
