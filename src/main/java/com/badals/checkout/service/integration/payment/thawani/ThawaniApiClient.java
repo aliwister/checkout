f866014cb6bd3a5eb63189209a5c15fb0053346b
Ebi <tmpmailebi@gmail.com>
2024-03-27 22:49:07 +0330

    added thawani createPaymentIntent request

--- src/main/java/com/badals/checkout/service/integration/payment/thawani/ThawaniApiClient.java
+++ src/main/java/com/badals/checkout/service/integration/payment/thawani/ThawaniApiClient.java
@@ -2,8 +2,10 @@ package com.badals.checkout.service.integration.payment.thawani;
 
 import com.badals.checkout.service.integration.payment.thawani.dto.request.CreateCheckoutBody;
 import com.badals.checkout.service.integration.payment.thawani.dto.request.CreateCustomerBody;
+import com.badals.checkout.service.integration.payment.thawani.dto.request.CreatePaymentIntentBody;
 import com.badals.checkout.service.integration.payment.thawani.dto.response.CreateCheckoutSession;
 import com.badals.checkout.service.integration.payment.thawani.dto.response.CreateCustomer;
+import com.badals.checkout.service.integration.payment.thawani.dto.response.CreatePaymentIntentResponse;
 import com.badals.checkout.service.integration.payment.thawani.dto.response.ThawaniResponse;
 import feign.RequestLine;
 
@@ -14,4 +16,7 @@ public interface ThawaniApiClient {
 
     @RequestLine("POST /checkout/session")
     ThawaniResponse<CreateCheckoutSession> createCheckoutSession(CreateCheckoutBody request);
+
+    @RequestLine("POST /payment_intents")
+    ThawaniResponse<CreatePaymentIntentResponse> createPaymentIntent(CreatePaymentIntentBody request);
 }
