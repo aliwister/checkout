6b2e37cc325fdfaacab67c4a7e981a3b14699af0
Ebi <tmpmailebi@gmail.com>
2024-04-08 10:09:00 +0330

    fixed empty metadata error

--- src/main/java/com/badals/checkout/xtra/systems/ThawaniPay.java
+++ src/main/java/com/badals/checkout/xtra/systems/ThawaniPay.java
@@ -6,6 +6,7 @@ import com.badals.checkout.service.LockedCartException;
 import com.badals.checkout.service.TenantCheckoutService;
 import com.badals.checkout.service.dto.CartDTO;
 import com.badals.checkout.service.integration.payment.thawani.ThawaniPaymentService;
+import com.badals.checkout.service.integration.payment.thawani.dto.request.CreateCheckoutMetaData;
 import com.badals.checkout.service.integration.payment.thawani.dto.request.CreateCheckoutSessionBody;
 import com.badals.checkout.service.integration.payment.thawani.dto.request.Product;
 import com.badals.checkout.service.integration.payment.thawani.dto.response.CreateCheckoutSessionResponse;
@@ -14,14 +15,16 @@ import com.badals.checkout.xtra.PaymentSystem;
 import com.badals.checkout.xtra.PaymentType;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class ThawaniPay extends PaymentSystem {
-
     private TenantCheckoutService cartService;
     private ThawaniPaymentService thawaniPaymentService;
     private TenantCheckoutService checkoutService;
@@ -40,17 +43,31 @@ public class ThawaniPay extends PaymentSystem {
         try {
             String baseUrl = TenantCheckoutService.buildProfileBaseUrl(checkoutService.getTenant());
             cart = cartService.findBySecureKeyWithLock(secureKey);
+            List<Product> products = cart.getItems().stream().map(item ->
+                                                                    Product.builder()
+                                                                            .name(item.getName())
+                                                                            .quantity(item.getQuantity().intValue())
+                                                                            .unit_amount((item.getPrice().multiply(new BigDecimal(1000))).intValue())
+                                                                            .build()).collect(Collectors.toList());
+            if(cart.getCarrierRate() != null)
+                products.add(
+                        Product.builder()
+                                .name("shipping")
+                                .quantity(1)
+                                .unit_amount(Double.valueOf(cart.getCarrierRate()*1000).intValue())
+                                .build()
+                );
+
             CreateCheckoutSessionBody createCheckoutSessionBody = CreateCheckoutSessionBody.builder()
                     .client_reference_id(cart.getId().toString())
                     .mode("payment")
-                    .products(cart.getItems().stream().map(item -> Product.builder()
-                            .name(item.getName())
-                            .quantity(item.getQuantity().intValue())
-                            .unit_amount((item.getPrice().multiply(new BigDecimal(1000))).intValue())
-                            .build()).collect(Collectors.toList()))
+                    .products(products)
                     .success_url(baseUrl + "/checkout/callbacks/thawani/success")
                     .cancel_url(baseUrl + "/checkout/callbacks/thawani/failure")
-                    .metadata(null)
+                    .metadata(CreateCheckoutMetaData.builder()
+                            .client_checkout_id("")
+                            .client_customer_id("")
+                            .build())
                     .build();
 
             ThawaniResponse<CreateCheckoutSessionResponse> createCheckoutSessionResponse =
@@ -61,7 +78,11 @@ public class ThawaniPay extends PaymentSystem {
             return paymentDeclined(e.getMessage());
         } catch (LockedCartException e) {
             return paymentDeclined(e.getMessage());
-        } finally {
+        } catch (Exception e){
+            e.printStackTrace();
+            return paymentDeclined(e.getMessage());
+        }
+        finally {
             if(cart != null)
                 cartService.unlock(secureKey);
         }
