ec12b3f6969804a74d8a8d001b51c3221e874d7e
Ebi <tmpmailebi@gmail.com>
2024-05-08 23:22:23 +0330

    fixed antmatcher pattern

--- src/main/java/com/badals/checkout/SecurityConfiguration.java
+++ src/main/java/com/badals/checkout/SecurityConfiguration.java
@@ -71,7 +71,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/test/**").permitAll()
                 .antMatchers("/api/checkout/**/**").permitAll()
             .antMatchers("/api/register").permitAll()
-            .antMatchers("/stripe-support/**").permitAll()
+            .antMatchers("/api/stripe-support/**").permitAll()
             .antMatchers("/thawani-support/**").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/graphql").permitAll()
